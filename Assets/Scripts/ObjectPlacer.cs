de772ee17bfd2bd34b7b6a5d133b036e3af7f8a2
NBK <1justforstuding1@gmail.com>
2022-07-24 23:46:38 +0300

    add exhibit objects

--- Assets/Scripts/ObjectPlacer.cs
+++ Assets/Scripts/ObjectPlacer.cs
@@ -1,6 +1,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.XR.ARFoundation;
+using UnityEngine.XR.ARSubsystems;
 
 [RequireComponent(typeof(ARRaycastManager))]
 public class ObjectPlacer : MonoBehaviour
@@ -18,10 +19,47 @@ public class ObjectPlacer : MonoBehaviour
         _arRaycastManager = GetComponent<ARRaycastManager>();
     }
 
+    private void Update()
+    {
+        UpdatePlacementPose();   
+        if (Input.touchCount==2)
+            SetObject();
+    }
+
+    private void UpdatePlacementPose()
+    {
+        Vector2 screenCenter = _camera.ViewportToScreenPoint(new Vector2(0.5f,0.5f));
+        var ray = _camera.ScreenPointToRay(screenCenter);
+        if (Physics.Raycast(ray, out RaycastHit raycastHit))
+        {
+            SetObjectPosition(raycastHit.point);
+        }
+        else if (_arRaycastManager.Raycast(screenCenter, _hits, TrackableType.PlaneWithinPolygon))
+        {
+            SetObjectPosition(_hits[0].pose.position);
+        }
+    }
+
+    private void SetObject()
+    {
+        _installedObject.GetComponent<Collider>().enabled = true;
+        _installedObject.transform.parent = _container.transform;
+        _objectPlace=null;
+    }
+
+    private void SetObjectPosition(Vector3 position )
+    {
+        _objectPlace.position = position;
+        Vector3 cameraForward = _camera.transform.forward;
+        Vector3 cameraRotation = new Vector3(cameraForward.x, 0, cameraForward.z);
+        _objectPlace.rotation = Quaternion.Euler(cameraRotation);
+    }
+
     public void SetInstallObject(ItemData itemData)
     {
         if(_installedObject!=null)
             Destroy(_installedObject);
         _installedObject = Instantiate(itemData.Prefab, _objectPlace);
+        _installedObject.GetComponent<Collider>().enabled = false;
     }
 }
